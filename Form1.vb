8cf67e90de4d978b9acf5d0e5d0b22d940b8c02a
Abdallah Mohamed <abdallah.mohamed@kyocera-avx.com>
2023-07-31 12:00:15 -0400

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClickThis.Click
+        If lblHelloWorld.Text <> "" Then
+            lblHelloWorld.Text = ""
+        Else
+            lblHelloWorld.Text = "Hello World!"
+        End If
+    End Sub
+End Class
+
+
